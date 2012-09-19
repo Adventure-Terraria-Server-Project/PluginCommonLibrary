6e717c7a5d5f8434e6eecf3bcfd7868b3e6e8cbc
CoderCow <CoderCow@gmx.de>
2012-09-19 11:31:06 +0200

    Added StringCollection class. Added a TShock extension to easy the handling of command parameters

---
+++ StringCollection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+
+namespace Terraria.Plugins.CoderCow {
+  public class StringCollection: Collection<string> {
+    #region [Method: Constructor]
+    public StringCollection() {}
+
+    public StringCollection(IList<string> wrappedList): base(wrappedList) {}
+    #endregion
+
+    #region [Method: Contains, IndexOf]
+    public int IndexOf(string item, StringComparison comparison = StringComparison.InvariantCulture) {
+      if (comparison == StringComparison.InvariantCulture)
+        return base.IndexOf(item);
+
+      for (int i = 0; i < this.Items.Count; i++) {
+        if (this.Items[i].Equals(item, comparison))
+          return i;
+      }
+
+      return -1;
+    }
+
+    public bool Contains(string item, StringComparison comparison = StringComparison.InvariantCulture) {
+      return (this.IndexOf(item, comparison) > -1);
+    }
+    #endregion
+
+    #region [Method: ToString]
+    public override string ToString() {
+      return string.Join(", ", this.Items);
+    }
+    #endregion
+  }
+}
--- TShockEx.cs
+++ TShockEx.cs
@@ -7,11 +7,12 @@
 using System;
 using System.Diagnostics;
 using System.Diagnostics.Contracts;
-
+using System.Text;
 using TShockAPI;
 
 namespace Terraria.Plugins.CoderCow {
   public static class TShockEx {
+    #region [TSPlayer Extensions]
     public static TSPlayer GetPlayerByName(
       string userAccountName, StringComparison stringComparison = StringComparison.InvariantCulture
     ) {
@@ -26,5 +27,20 @@ namespace Terraria.Plugins.CoderCow {
     public static void SendTileSquareEx(this TSPlayer player, int x, int y, int size = 10) {
       player.SendData(PacketTypes.TileSendSquare, string.Empty, size, x, y);
     }
+    #endregion
+
+    #region [CommandArgs Extensions]
+    public static string ParamsToSingleString(this CommandArgs args, int fromIndex = 0) {
+      StringBuilder builder = new StringBuilder();
+      for (int i = fromIndex; i < args.Parameters.Count; i++) {
+        if (i > fromIndex)
+          builder.Append(' ');
+
+        builder.Append(args.Parameters[i]);
+      }
+
+      return builder.ToString();
+    }
+    #endregion
   }
 }

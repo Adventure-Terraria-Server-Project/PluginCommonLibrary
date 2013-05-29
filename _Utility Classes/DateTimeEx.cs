f980c6fa5e6bad4f42c552de0f6329c1fd8a2c15
CoderCow <CoderCow@gmx.de>
2013-05-29 13:16:34 +0200

    Added extension method DateTime.ToUnixTime()

---
+++ _Utility Classes/DateTimeEx.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace Terraria.Plugins.Common {
+  public static class DateTimeEx {
+    public static DateTime UnixTimeStamp = new DateTime(1970, 1, 1);
+    public static int ToUnixTime(this DateTime dateTime) {
+      return (int)(dateTime - DateTimeEx.UnixTimeStamp).TotalSeconds;
+    }
+  }
+}

1950648a0b70e57cef564d07ee530a43ddec6332
CoderCow <CoderCow@gmx.de>
2013-09-11 16:37:30 +0200

    Refined AsyncWorkQueue now allowing for multiple worker threads, now assigns names to the threads, anded more overloads to add worker tasks, improved exception handling.

--- _Threading/AsyncWorkQueue.cs
+++ _Threading/AsyncWorkQueue.cs
@@ -2,20 +2,30 @@
 using System.Diagnostics;
 using System.Linq;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Terraria.Plugins.Common {
+  public delegate void QueueExceptionHandler(Task task, object taskState, AggregateException exception);
+
   public class AsyncWorkQueue: IDisposable {
+    #region [Nested: WorkItem]
     private struct WorkItem {
       public static readonly WorkItem Invalid = default(WorkItem);
 
-      #region [Property: CompletionSource]
-      private readonly TaskCompletionSource<object> completionSource;
+      #region [Property: ResultType]
+      private readonly Type resultType;
 
-      public TaskCompletionSource<object> CompletionSource {
+      public Type ResultType {
+        get { return this.resultType; }
+      }
+      #endregion
+
+      #region [Property: CompletionSource]
+      private readonly dynamic completionSource;
+
+      public dynamic CompletionSource {
         get { return this.completionSource; }
       }
       #endregion
@@ -29,99 +39,146 @@ namespace Terraria.Plugins.Common {
       #endregion
 
       #region [Property: Function]
-      private readonly Func<object,object> function;
+      private readonly dynamic function;
 
-      public Func<object,object> Function {
+      public dynamic Function {
         get { return this.function; }
       }
       #endregion
 
       #region [Property: State]
-      private readonly object state;
+      private readonly dynamic state;
 
-      public object State {
+      public dynamic State {
         get { return this.state; }
+      }
+      #endregion
+
+      #region [Property: ExceptionHandler]
+      private readonly QueueExceptionHandler exceptionHandler;
+
+      public QueueExceptionHandler ExceptionHandler {
+        get { return this.exceptionHandler; }
       }
       #endregion
 
 
       #region [Method: Constructor]
       public WorkItem(
-        TaskCompletionSource<object> completionSource, CancellationToken? cancellationToken, Func<object,object> function,
-        object state = null
+        dynamic completionSource, CancellationToken? cancellationToken, dynamic function, dynamic state = null,
+        QueueExceptionHandler exceptionHandler = null
       ) {
-        Contract.Requires<ArgumentNullException>(completionSource != null);
-        Contract.Requires<ArgumentNullException>(function != null);
+        //Contract.Requires<ArgumentNullException>(completionSource != null);
+        //Contract.Requires<ArgumentNullException>(function != null);
 
         this.completionSource = completionSource;
         this.cancellationToken = cancellationToken;
         this.function = function;
+        this.resultType = null;
         this.state = state;
+        this.exceptionHandler = exceptionHandler;
       }
       #endregion
     }
+    #endregion
 
-    #region [Property: WorkerTimeoutMs]
-    private readonly int workerTimeoutMs;
-
-    public int WorkerTimeoutMs {
-      get { return this.workerTimeoutMs; }
+    #region [Property: WorkerThreadCount]
+    public int WorkerThreadCount {
+      get { return this.workers.Length; }
     }
     #endregion
 
-    private Task worker;
-    private CancellationTokenSource workerTokenSource;
+    private readonly Thread[] workers;
+    private readonly int workerTimeoutMs;
+    private readonly CancellationTokenSource workerTokenSource;
     private readonly BlockingCollection<WorkItem> queuedItems;
 
 
     #region [Method: Constructor]
-    public AsyncWorkQueue() {
+    public AsyncWorkQueue(
+      string threadName = "Async Work Queue Thread", ThreadPriority threadPriority = ThreadPriority.BelowNormal,
+      int workerThreadCount = 1
+    ) {
       this.workerTimeoutMs = -1;
+      this.workerTokenSource = new CancellationTokenSource();
       this.queuedItems = new BlockingCollection<WorkItem>();
+      this.workers = new Thread[workerThreadCount];
 
-      this.StartWorker();
+      for (int i = 0; i < workerThreadCount; i++) {
+        Thread worker = new Thread(this.ProcessWorkItems) { IsBackground = false };
+        this.workers[i] = worker;
+
+        worker.Name = threadName;
+        worker.Priority = threadPriority;
+        worker.Start();
+      }
     }
     #endregion
 
-    #region [Methods: EnqueueTask, AwaitAll, ProcessWorkItems]
-    public Task<object> EnqueueTask(
-      Func<object,object> function, object state = null, CancellationToken? cancellationToken = null
+    #region [Methods: EnqueueTask, AwaitAll]
+    public Task EnqueueTask(Action action, CancellationToken? cancellationToken = null, 
+      QueueExceptionHandler exceptionHandler = null
     ) {
       Contract.Requires<ObjectDisposedException>(!this.IsDisposed);
 
       var completionSource = new TaskCompletionSource<object>();
-      this.queuedItems.Add(new WorkItem(completionSource, cancellationToken, function, state));
+      this.queuedItems.Add(new WorkItem(completionSource, cancellationToken, action, null, exceptionHandler));
       
       return completionSource.Task;
     }
 
-    public void AwaitAll(TimeSpan timeout) {
+    public Task EnqueueTask<TState>(
+      Action<TState> action, TState state, CancellationToken? cancellationToken = null,
+      QueueExceptionHandler exceptionHandler = null
+    ) {
       Contract.Requires<ObjectDisposedException>(!this.IsDisposed);
 
-      this.AwaitAllInternal(timeout);
+      var completionSource = new TaskCompletionSource<object>();
+      this.queuedItems.Add(new WorkItem(completionSource, cancellationToken, action, state, exceptionHandler));
+      
+      return completionSource.Task;
     }
 
-    public void AwaitAll() {
+    public Task<TResult> EnqueueTask<TResult>(
+      Func<TResult> function, CancellationToken? cancellationToken = null, QueueExceptionHandler exceptionHandler = null
+    ) {
       Contract.Requires<ObjectDisposedException>(!this.IsDisposed);
 
-      this.AwaitAllInternal(TimeSpan.FromMilliseconds(1)); // Infinite
+      var completionSource = new TaskCompletionSource<TResult>();
+      this.queuedItems.Add(new WorkItem(completionSource, cancellationToken, function, null, exceptionHandler));
+      
+      return completionSource.Task;
     }
 
-    private void AwaitAllInternal(TimeSpan timeout) {
+    public Task<TResult> EnqueueTask<TState,TResult>(
+      Func<TState,TResult> function, TState state, CancellationToken? cancellationToken = null,
+      QueueExceptionHandler exceptionHandler = null
+    ) {
+      Contract.Requires<ObjectDisposedException>(!this.IsDisposed);
+
+      var completionSource = new TaskCompletionSource<TResult>();
+      this.queuedItems.Add(new WorkItem(completionSource, cancellationToken, function, state, exceptionHandler));
+      
+      return completionSource.Task;
+    }
+
+    public void AwaitAll(int timeoutMs = -1) {
+      Contract.Requires<ObjectDisposedException>(!this.IsDisposed);
+
+      this.AwaitAllInternal(timeoutMs);
+    }
+
+    private void AwaitAllInternal(int timeoutMs) {
       Task[] itemTasks = new Task[this.queuedItems.Count];
       int counter = 0;
       foreach (Task<object> itemTask in this.queuedItems.Select(item => item.CompletionSource.Task))
         itemTasks[counter++] = itemTask;
 
-      Task.WaitAll(itemTasks, timeout);
+      Task.WaitAll(itemTasks, timeoutMs);
     }
+    #endregion
 
-    private void StartWorker() {
-      this.workerTokenSource = new CancellationTokenSource();
-      this.worker = new Task(this.ProcessWorkItems, this.workerTokenSource.Token, TaskCreationOptions.LongRunning);
-      this.worker.Start();
-    }
-
+    #region [ProcessWorkItems]
     private void ProcessWorkItems() {
       try {
         WorkItem currentItem;
@@ -133,22 +190,42 @@ namespace Terraria.Plugins.Common {
             currentItem.CompletionSource.SetCanceled();
           } else {
             try {
-              object result = currentItem.Function(currentItem.State);
-              currentItem.CompletionSource.SetResult(result); // Signal completed
+              dynamic result = null;
+              if (currentItem.Function is Action)
+                currentItem.Function();
+              else if (currentItem.Function.GetType().GetGenericTypeDefinition().FullName.StartsWith("System.Func`1"))
+                result = currentItem.Function();
+              else if (currentItem.Function.GetType().GetGenericTypeDefinition().FullName.StartsWith("System.Action`1"))
+                currentItem.Function(currentItem.State);
+              else
+                result = currentItem.Function(currentItem.State);
+
+              // Note: This will also execute all continuing tasks.
+              currentItem.CompletionSource.SetResult(result);
             } catch (OperationCanceledException ex) {
               if (ex.CancellationToken == currentItem.CancellationToken)
                 currentItem.CompletionSource.SetCanceled();
               else
                 currentItem.CompletionSource.SetException(ex);
-            } catch (Exception ex) {
+            } catch (AggregateException ex) {
               currentItem.CompletionSource.SetException(ex);
+              if (currentItem.ExceptionHandler != null) {
+                try {
+                  currentItem.ExceptionHandler(currentItem.CompletionSource.Task, currentItem.State, ex);
+                } catch (Exception exInner) {
+                  Debug.WriteLine("An exception handler of an enqueued work item has thrown an exception: \n" + exInner);
+                }
+              }
             }
           }
         }
       } catch (OperationCanceledException) {
         // Thrown when "this.workerTokenSource.Token" token is set.
+        return;
+      } catch (ThreadAbortException) {
+        return;
       } catch (Exception ex) {
-        Debug.WriteLine("Async Work Queue worker has thrown an unexpected exception: \n" + ex);
+        Debug.WriteLine("{0} has thrown an unexpected exception: \n{1}", Thread.CurrentThread.Name, ex);
       }
     }
     #endregion
@@ -169,16 +246,17 @@ namespace Terraria.Plugins.Common {
         if (this.workerTokenSource != null) {
           this.workerTokenSource.Cancel();
 
-          try {
-            this.worker.Wait(TimeSpan.FromSeconds(30));
-          } catch (AggregateException) {}
+          for (int i = 0; i < this.workers.Length; i++) {
+            try {
+              this.workers[i].Join(TimeSpan.FromSeconds(30));
+            } catch (AggregateException) {
+              this.workers[i].Abort();
+            }
+          }
         }
 
         if (this.workerTokenSource != null)
           this.workerTokenSource.Dispose();
-
-        if (this.worker != null)
-          this.worker.Dispose();
       }
     }
 

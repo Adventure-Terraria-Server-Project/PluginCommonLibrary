5900e118e5060ab98e2529f1339b0d2352e3c8ac
CoderCow <CoderCow@gmx.de>
2013-09-11 16:37:56 +0200

    Added EntityVersion management to DatabaseHandler.

--- _Data/DatabaseHandlerBase.cs
+++ _Data/DatabaseHandlerBase.cs
@@ -58,17 +58,76 @@ namespace Terraria.Plugins.Common {
 
           break;
         default:
-          throw new NotImplementedException();
+          throw new NotSupportedException("Not supported storage type.");
       }
     }
 
-    public abstract void EnsureDataStructure();
+    public virtual void EnsureDataStructure() {
+      if (this.SqlType == SqlType.Sqlite) {
+        this.DbConnection.Query(string.Concat(
+          @"CREATE TABLE IF NOT EXISTS EntityVersion (",
+          @"  Name varchar(64) NOT NULL,",
+          @"  Version tinyint UNSIGNED NOT NULL DEFAULT 1,",
+          @"  PRIMARY KEY (Name)",
+          @");"
+        ));
+      } else if (this.SqlType == SqlType.Mysql) {
+        this.DbConnection.Query(string.Concat(
+          @"CREATE TABLE IF NOT EXISTS EntityVersion (",
+          @"  Name varchar(64) NOT NULL,",
+          @"  Version tinyint UNSIGNED NOT NULL DEFAULT 1,",
+          @"  PRIMARY KEY (Name)",
+          @") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;"
+        ));
+      } else {
+        throw new NotSupportedException("Not supported storage type.");
+      }
+
+      this.AddOrUpdateEntityVersion("EntityVersion", 1);
+    }
 
     protected IQueryBuilder GetQueryBuilder() {
       if (this.SqlType == SqlType.Sqlite)
         return new SqliteQueryCreator();
       else
         return new MysqlQueryCreator();
+    }
+    #endregion
+
+    #region [Utility Methods]
+    protected bool CheckTableExists(string tableName) {
+      if (this.SqlType == SqlType.Sqlite) {
+        const string QueryString = @"SELECT name FROM sqlite_master WHERE type='table' AND name=@0";
+        using (QueryResult reader = this.DbConnection.QueryReader(QueryString, tableName))
+          return reader.Read();
+      } else if (this.SqlType == SqlType.Mysql) {
+        const string QueryString = @"SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA=@0 AND TABLE_NAME=@1";
+        using (QueryResult reader = this.DbConnection.QueryReader(QueryString, tableName, this.DbConnection.Database))
+          return reader.Read();
+      } else {
+        throw new NotSupportedException("Not supported storage type.");
+      }
+    }
+
+    protected bool TryGetEntityVersion(string name, out byte version) {
+      const string QueryString = @"SELECT Version FROM EntityVersion WHERE Name = @0;";
+      using (QueryResult reader = this.DbConnection.QueryReader(QueryString, name)) {
+        if (!reader.Read()) {
+          version = 0;
+          return false;
+        }
+
+        version = reader.Get<byte>("Version");
+        return true;
+      }
+    }
+
+    protected void AddOrUpdateEntityVersion(string name, byte version) {
+      const string QueryString = 
+        @"  INSERT OR IGNORE INTO EntityVersion (Name, Version) VALUES (@0, @1);" + 
+        @"  UPDATE EntityVersion SET Version = @1 WHERE Name = @0 AND Version < @1;";
+
+      this.DbConnection.Query(QueryString, name, version);
     }
     #endregion
 

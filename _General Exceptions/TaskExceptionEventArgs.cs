caed2f94459b5e517dbb16da375ad1d87531ca47
CoderCow <CoderCow@gmx.de>
2013-09-11 16:38:53 +0200

    Added TaskExceptionEventArgs.

---
+++ _General Exceptions/TaskExceptionEventArgs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Threading.Tasks;
+
+namespace Terraria.Plugins.Common {
+  public class TaskExceptionEventArgs: EventArgs {
+    #region [Property: Task]
+    private readonly Task task;
+
+    public Task Task {
+      get { return this.task; }
+    }
+    #endregion
+
+    #region [Property: TaskState]
+    private readonly object taskState;
+
+    public object TaskState {
+      get { return this.taskState; }
+    }
+    #endregion
+
+
+    #region [Method: Constructor]
+    public TaskExceptionEventArgs(Task task, object taskState = null) {
+      this.task = task;
+      this.taskState = taskState;
+    }
+    #endregion
+  }
+}

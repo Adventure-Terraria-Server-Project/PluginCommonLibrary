d86760f884747855eeba10fa51b9a3d2d0b60b36
Stealownz <stealownz@gmail.com>
2013-10-25 00:24:21 +0800

    updated NPCs and Item List

--- TerrariaUtils.cs
+++ TerrariaUtils.cs
@@ -11,7 +11,7 @@ namespace Terraria.Plugins.Common {
     public const int WallType_Max = 111;
     public const int ItemType_Min = -48;
     public const int ItemType_Max = 1613;
-    public const int NpcType_Min = -17;
+    public const int NpcType_Min = -65;
     public const int NpcType_Max = 296;
     public const int DefaultTextureTileSize = 18;
     public const int TileSize = 16;

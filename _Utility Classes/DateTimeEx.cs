0bb67700df3b39873d57f6ff5d583dcbbb35dae5
CoderCow <CoderCow@gmx.de>
2015-02-06 21:48:24 +0100

    Fixes a possible date time issue related with json serialization.

--- _Utility Classes/DateTimeEx.cs
+++ _Utility Classes/DateTimeEx.cs
@@ -3,7 +3,7 @@ using System.Diagnostics.Contracts;
 
 namespace Terraria.Plugins.Common {
   public static class DateTimeEx {
-    public static DateTime UnixTimeStamp = new DateTime(1970, 1, 1);
+    public static DateTime UnixTimeStamp = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
     public static int ToUnixTime(this DateTime dateTime) {
       return (int)(dateTime - DateTimeEx.UnixTimeStamp).TotalSeconds;
     }

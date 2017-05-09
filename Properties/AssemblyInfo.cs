a014c307a0cb3a985f72be4594a27f681fa71946
CoderCow <CoderCow@gmx.de>
2017-05-09 02:09:40 +0200

    chore(version): tick version 4.0.2

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("0f67f6cb-d020-4a2f-8c59-035e3897fe48")]
 
-[assembly: AssemblyVersion("4.0.1.0")]
-[assembly: AssemblyFileVersion("4.0.1.0")]
+[assembly: AssemblyVersion("4.0.2.0")]
+[assembly: AssemblyFileVersion("4.0.2.0")]
 
-[assembly: AssemblyInformationalVersion("4.0.1+4.Branch.master.Sha.31daa3e6a5bf40cb4e6f077cf694a31e0888f33d")]
+[assembly: AssemblyInformationalVersion("4.0.2+5.Branch.master.Sha.f059e4846a55462f49282b2cea136b369abff304")]

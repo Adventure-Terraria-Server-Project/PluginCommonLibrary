43cb1c583413f3af80b62163d109dc58bd2d9d00
Stealownz <stealownz@gmail.com>
2020-06-03 01:18:50 +0800

    chore(version): tick version 4.1.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("0f67f6cb-d020-4a2f-8c59-035e3897fe48")]
 
-[assembly: AssemblyVersion("4.0.3.0")]
-[assembly: AssemblyFileVersion("4.0.3.0")]
+[assembly: AssemblyVersion("4.1.0.0")]
+[assembly: AssemblyFileVersion("4.1.0.0")]
 
-[assembly: AssemblyInformationalVersion("4.0.3+1.Branch.master.Sha.a3eda0ec7c8d2a39c63a1bff8fad073f28bf6b80")]
+[assembly: AssemblyInformationalVersion("4.1.0+20.Branch.master.Sha.458ae7b227a8c6016e314a1b1355c17e812cbe1d")]

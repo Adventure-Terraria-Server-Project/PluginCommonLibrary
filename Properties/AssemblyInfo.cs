d061839ef15ef69414d69939299d0c478c6a7eba
Stealownz <stealownz@gmail.com>
2021-05-25 00:42:33 +0800

    chore(version): tick version 4.2.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("CoderCow's Plugin Common Library")]
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("0f67f6cb-d020-4a2f-8c59-035e3897fe48")]
 
-[assembly: AssemblyVersion("4.1.2.0")]
-[assembly: AssemblyFileVersion("4.1.2.0")]
+[assembly: AssemblyVersion("4.2.0.0")]
+[assembly: AssemblyFileVersion("4.2.0.0")]
 
-[assembly: AssemblyInformationalVersion("4.1.2+2.Branch.master.Sha.21e70115924c23a7ac35af6512642ec764c50f77")]
+[assembly: AssemblyInformationalVersion("4.2.0+4.Branch.master.Sha.64d8f69ff3dd8f377077cced23d4151bc3048e88")]
